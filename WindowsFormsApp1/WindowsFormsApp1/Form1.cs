b3a3c528d63d6fd9f6b93b735569c579618e165c
kiridumeuta <kiridume1125@gmail.com>
2026-02-12 16:29:40 +0900

    a

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -22,7 +22,7 @@ namespace WindowsFormsApp1
         private void Form1_Load(object sender, EventArgs e)
         {
             //string connStr = "server=172.16.2.26;user id=自分のid;password=自分のパスワード;database=自分のデータベース名";
-            string connStr = "server=172.16.2.26;user id=tateno;password=ae21215926;database=uta";
+            string connStr = "server=172.16.2.26;user id=root;password=ae21215926;database=uta";
             MySqlConnection conn = new MySqlConnection(connStr);
             try
             {
@@ -44,17 +44,19 @@ namespace WindowsFormsApp1
             string str0 = textBox1.Text;
             string str1 = textBox2.Text;
             //int str2 = int.Parse(textBox3.Text);
-            string str3 = textBox4.Text;
-            string connStr = "server=172.16.2.26;user id=tateno;password=ae21215926;database=uta";
+            string str3 = textBox3.Text;
+            string connStr = "server=172.16.2.26;user id=root;password=ae21215926;database=uta";
             MySqlConnection conn = new MySqlConnection(connStr);
             try
             {
                 conn.Open();
                 DataTable tbl = new DataTable();
                 DataTable tbl1 = new DataTable();
-                MySqlDataAdapter dataAdp = new MySqlDataAdapter("update ITTest set ITTest=" + str3 + " where id = " + str0, conn);
+                MySqlDataAdapter dataAdp = new MySqlDataAdapter("update ITTest set name=" + str3 + " where id = " + str0, conn);
+                MySqlDataAdapter dataAdp2 = new MySqlDataAdapter("update ITTest set name1=" + str1 + " where id = " + str0, conn);
                 dataAdp.Fill(tbl);
                 MySqlDataAdapter dataAdp1 = new MySqlDataAdapter("select * from ITTest", conn);
+                MySqlDataAdapter dataAdp4 = new MySqlDataAdapter("select * from ITTest", conn);
                 dataAdp1.Fill(tbl1);
                 dataGridView1.DataSource = tbl1;
                 conn.Close();
